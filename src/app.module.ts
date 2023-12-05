346aa9cb857d8b614ad3d1611cf69ef90d542962
leeseungjun <cjstkrak@gmail.com>
2023-12-05 21:31:49 +0900

    add: typeorm모듈 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,33 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chats } from './entities/Chats';
+import { Users } from './entities/Users';
+import { Invites } from './entities/Invites';
+import { UserInSpace } from './entities/UserInSpace';
+import { Spaces } from './entities/Spaces';
 
 @Module({
-  imports: [AuthModule, UserModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [Chats, Users, Invites, UserInSpace, Spaces, Users],
+      synchronize: true,
+      keepConnectionAlive: true,
+      logging: true,
+      charset: 'utf8mb4',
+      // 이모티콘 쓰기 위해
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
