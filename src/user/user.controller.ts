cd875114e4b603a69150b40cb1f04ffdd640c8f8
leeseungjun <cjstkrak@gmail.com>
2023-12-05 13:40:04 +0900

    feat: hot reload 설정
    웹팩사용

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  // @Get()
+  // getUsers(@User() user) {
+  //   return user || false;
+  // }
+  //   @Post()
+  //   async join(@Body() body: JoinRequestDto) {
+  //     await this.userService.join(body.email, body.nickname, body.password);
+  //   }
+
+  //   @Post('login')
+  //   logIn(@User() user) {
+  //     return user;
+  //   }
+  //   @Post('logout')
+  //   logOut(@Req() req, @Res() res) {
+  //     req.logOut();
+  //     res.clearCookie('connect.sid', { httpOnly: true });
+  //     res.send('ok');
+  //   }
+}
