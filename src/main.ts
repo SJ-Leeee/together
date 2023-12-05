f4d6808f8e001886958c17546f4814d65626205a
leeseungjun <cjstkrak@gmail.com>
2023-12-05 14:19:30 +0900

    feat: env 파일설정

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-
+  const test = process.env.TEST;
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+  console.log(`${test}포트 오픈`);
 }
 bootstrap();
